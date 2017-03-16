d36106c2cdc879e1bb39b867456bf0410044ec52
Jesse Norris <jraenorris.developer@gmail.com>
2017-03-16 02:42:25 -0400

    Styling changes

--- react/src/components/Layout.js
+++ react/src/components/Layout.js
@@ -5,14 +5,15 @@ import { Row, Column } from 'react-foundation';
 const Layout = (props) => {
   return(
     <div>
-      <div className="top-bar">
-        <div className="top-bar-title">
-          <strong><a>Site Title</a></strong>
+      <div className="top-bar small-nav">
+        <div className="top-bar-title text-center">
+          <h1 className="lt-off-white-text">Jesse & David</h1>
+          <div className="date">1.27.2018</div>
 
         </div>
       </div>
-      <Row className="react-layout">
-        <Column small={12} medium={10} offsetOnMedium={1}>
+      <Row className="react-layout text-center">
+        <Column small={12} medium={10} offsetOnMedium={1} className="small-opaque">
           { props.children }
         </Column>
       </Row>
