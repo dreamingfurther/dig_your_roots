fc0ae51c04bb441d904de420ed7e34546389c6f3
Jesse Norris <jraenorris.developer@gmail.com>
2017-05-03 22:04:49 -0400

    FA Icon Mobile Nav

--- react/src/components/NavBarLinks.js
+++ react/src/components/NavBarLinks.js
@@ -3,9 +3,18 @@ import { Link } from 'react-router';
 import { Row, Column, Menu, MenuItem } from 'react-foundation';
 
 const NavBarLinks = (props) => {
+  let eventsFA = (<i className="fa fa-calendar-check-o" aria-hidden="true"></i>)
+  let vipFA = (
+    <span className="fa-stack">
+      <i className="fa fa-star fa-stack-2x"></i>
+      <i className="fa fa-user fa-stack-1x fa-inverse"></i>
+    </span>
+  )
+  let signOutFA = (<i className="fa fa-sign-out" aria-hidden="true"></i>)
+
   let fancyNavLink = (path, text) => {
     let linkHelper;
-    return linkHelper = (text == 'Sign out') ?
+    return linkHelper = (text == 'Sign out' || text == signOutFA) ?
       <Link id="sign-out-link" onClick={ props.signOut }>{ text }</Link> :
       <Link to={ path } >{ text }</Link>
   }
@@ -16,6 +25,11 @@ const NavBarLinks = (props) => {
         <MenuItem>{ fancyNavLink('/events', 'Your Events') }</MenuItem>
         <MenuItem>{ fancyNavLink('/vips', 'V.I.P.s') }</MenuItem>
         <MenuItem>{ fancyNavLink(null, 'Sign out') }</MenuItem>
+      </Menu>
+      <Menu className="columns show-for-small-only small-12 mobile-nav-icons">
+        <MenuItem>{ fancyNavLink('/events', eventsFA) }</MenuItem>
+        <MenuItem>{ fancyNavLink('/vips', vipFA) }</MenuItem>
+        <MenuItem>{ fancyNavLink(null, signOutFA) }</MenuItem>
       </Menu>
     </div>
   )
